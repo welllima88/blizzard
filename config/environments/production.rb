b3690821be85d553654daf31ef91c39630e99848
Steffan Perry <sperry1988@gmail.com>
2014-12-11 16:14:33 -0500

    update 12/11/2014 16:14:33

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'mail.thecatalystagency.com',
+    :port                 => 587,
+    :user_name            => 'support@evendra.com',
+    :password             => 'boomer1',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+  
+  
 end
